94feb9ca72d721c95355e5b1b5bc66a1e7316c11
sssemil <suleymanovemil8@gmail.com>
2014-09-20 15:40:48 +0500

    pollux: update fingerprint from 10.5.1.A.0.283
    Change-Id: Ide2e9f72b3551fce278ccc01f22f985bca189ac1

--- cm.mk
+++ cm.mk
@@ -9,7 +9,7 @@ $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 # Inherit CM common Phone stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=SGP321 BUILD_FINGERPRINT=Sony/SGP321_1273-0364/SGP321:4.2.2/10.3.1.A.0.244/C_93rg:user/release-keys PRIVATE_BUILD_DESC="SGP321-user 4.2.2 10.3.1.A.0.244 C_93rg test-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=SGP321 BUILD_FINGERPRINT=Sony/SGP321/SGP321:4.4.4/10.5.1.A.0.283/5P53rQ:user/release-keys PRIVATE_BUILD_DESC="SGP321-user 4.4.4 10.5.1.A.0.283 5P53rQ test-keys"
 
 PRODUCT_NAME := cm_pollux
 PRODUCT_DEVICE := pollux
