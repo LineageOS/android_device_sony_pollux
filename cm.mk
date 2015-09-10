afcbe6458994eed117820b98d1d4bd1960463714
Georg Veichtlbauer <georg.veichtlbauer@gmail.com>
2015-09-10 12:42:28 +0200

    update build fingerprint
    Change-Id: I172d3f03b71d5490fbc36ac4e989e691cd93094b

--- cm.mk
+++ cm.mk
@@ -6,7 +6,7 @@ $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 # Inherit CM common Phone stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=SGP321 BUILD_FINGERPRINT=Sony/SGP321/SGP321:5.0.2/10.6.A.0.454/1850661791:user/release-keys PRIVATE_BUILD_DESC="SGP321-user 5.0.2 10.6.A.0.454 1850661791 test-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=SGP321 BUILD_FINGERPRINT=Sony/SGP321/SGP321:5.1.1/10.7.A.0.222/2921970873:user/release-keys PRIVATE_BUILD_DESC="SGP321-user 5.1.1 10.7.A.0.222 2921970873 test-keys"
 
 PRODUCT_NAME := cm_pollux
 PRODUCT_DEVICE := pollux
