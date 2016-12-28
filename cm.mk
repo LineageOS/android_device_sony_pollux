0fada3501e505c5b9ff420a25d5b322583bb97d2
ZeeLog <Danjel.Zammit@gmail.com>
2016-12-28 19:40:56 +0100

    pollux: update build fingerprint & description
    * From 10.7.A.0.228
    
    Change-Id: I88cd2ea03026d3036b5455046fca869d585629d3

--- cm.mk
+++ cm.mk
@@ -3,7 +3,7 @@ $(call inherit-product, device/sony/pollux/full_pollux.mk)
 # Inherit CM common Phone stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=SGP321 BUILD_FINGERPRINT=Sony/SGP321/SGP321:5.1.1/10.7.A.0.222/2921970873:user/release-keys PRIVATE_BUILD_DESC="SGP321-user 5.1.1 10.7.A.0.222 2921970873 test-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=SGP321 BUILD_FINGERPRINT=Sony/SGP321/SGP321:5.1.1/10.7.A.0.228/2020576680:user/release-keys PRIVATE_BUILD_DESC="SGP321-user 5.1.1 10.7.A.0.228 2020576680 release-keys"
 
 PRODUCT_NAME := cm_pollux
 PRODUCT_DEVICE := pollux
