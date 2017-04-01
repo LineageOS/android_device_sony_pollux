841d105ef74d8c4a42890067dccbbc0d9d6436c1
Chippa-a <vusal1372@gmail.com>
2017-04-01 20:47:58 +0300

    pollux: Rebase the device tree into fragments structure
     * Based on new device tree structure introduced by
       Steve Kondik for Nextbit Robin (ether)
    
     * Also thanks to Adrian DC for his useful improvements
       introduced for huashan and blue-common devices
    
    Change-Id: I5e82813eab056a5e101cd64b7d49fb191c802d4a
    Signed-off-by: Chippa-a <vusal1372@gmail.com>

--- lineage.mk
+++ lineage.mk
@@ -1,9 +1,36 @@
-$(call inherit-product, device/sony/pollux/full_pollux.mk)
+#
+# Copyright (C) 2013 The CyanogenMod Project
+#           (C) 2017 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
-# Inherit common Phone stuff.
+# Inherit common LineageOS stuff
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=SGP321 BUILD_FINGERPRINT=Sony/SGP321/SGP321:5.1.1/10.7.A.0.228/2020576680:user/release-keys PRIVATE_BUILD_DESC="SGP321-user 5.1.1 10.7.A.0.228 2020576680 release-keys"
+# Inherit device configurations
+$(call inherit-product, device/sony/pollux/device.mk)
 
+# Device display
+TARGET_SCREEN_HEIGHT := 1200
+TARGET_SCREEN_WIDTH := 1920
+
+# Device identifications
 PRODUCT_NAME := lineage_pollux
 PRODUCT_DEVICE := pollux
+PRODUCT_BRAND := Sony
+PRODUCT_MANUFACTURER := Sony
+PRODUCT_MODEL := Xperia Tablet Z LTE
+
+# Build fingerprints
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=SGP321 BUILD_FINGERPRINT=Sony/SGP321/SGP321:5.1.1/10.7.A.0.228/2020576680:user/release-keys PRIVATE_BUILD_DESC="SGP321-user 5.1.1 10.7.A.0.228 2020576680 release-keys"
